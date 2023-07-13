10af52838dc609c6366cbdefab3490f7001eca37
Benjamin MARCHAND <cyberbobjr@gmail.com>
2023-07-13 16:07:45 +0200

    Implémentation mining block

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,4 +1,8 @@
 const {GENESIS_DATA} = require("../config");
+const {keccakHash} = require("../util");
+const HASH_LENGTH = 64;
+const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
 
 class Block {
 
@@ -6,8 +10,38 @@ class Block {
         this.blockHeaders = blockHeaders;
     }
 
-    static mineBlock({lastBlock}) {
+    static calculateBlockTargetHash({lastBlock}) {
+        const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
+        if (value.length > HASH_LENGTH) {
+            return 'f'.repeat(HASH_LENGTH);
+        }
+        return '0'.repeat(HASH_LENGTH - value.length) + value;
+    }
 
+    static mineBlock({lastBlock, beneficiary}) {
+        const target = Block.calculateBlockTargetHash({lastBlock});
+        let timestamp, truncatedBlockHeaders, header, nonce, underTargetHast;
+        do {
+            timestamp = Date.now();
+            truncatedBlockHeaders = {
+                parentHash: keccakHash(lastBlock.blockHeaders),
+                beneficiary,
+                difficulty: lastBlock.blockHeaders.difficulty + 1,
+                number: lastBlock.blockHeaders.number + 1,
+                timestamp
+            }
+            header = keccakHash(truncatedBlockHeaders);
+            nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+            underTargetHast = keccakHash(header + nonce);
+
+        } while (underTargetHast > target)
+
+        return new this({
+            blockHeaders: {
+                ...truncatedBlockHeaders,
+                nonce
+            }
+        })
     }
 
     static genesis() {
@@ -16,3 +50,10 @@ class Block {
 }
 
 module.exports = Block;
+
+const block = Block.mineBlock({
+    lastBlock: Block.genesis(),
+    beneficiary: 'foo'
+})
+
+console.log('block', block);
